0a950a88fb154c4f4a1c675a59a1e1656c3f6702
harsh-sikarwar <hvsikarwar@gmail.com>
2024-12-13 01:47:14 +0530

    3rd section push

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+
+let grandchild = document.getElementsByClassName('hvrc');
+let box = document.getElementsByClassName('olay');
+
+console.log(grandchild.length);
+console.log(box)
+
+for (let i = 0; i < grandchild.length; i++) {
+     grandchild[i].addEventListener('mouseenter', () => {
+        box[i].classList.add('hovered'); // Add the class when the grandchild is hovered
+      });}
+
+
+for (let i = 0; i < grandchild.length; i++) {
+     grandchild[i].addEventListener('mouseleave', () => {
+        box[i].classList.remove('hovered'); // Remove the class when the hover ends
+      })
+}
